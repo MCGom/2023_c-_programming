aeeefffe4effc5539c285363fed2518f78f47e40
YUHAN <YUHAN@PC5406_09>
2023-03-14 12:06:09 +0900

    초기 커밋.

---
+++ 2nd/sln_2/project_1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+
+namespace project_1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             * 출력과 관련된 연습
+            WriteLine("WriteLine");
+            WriteLine("헬로 월드!");
+            WriteLine("C# 프로그래밍");
+
+            Write("실습\n");
+            Write("화이팅");
+
+            Write("\n");
+            
+            Write("실습");
+            WriteLine();
+            Write("화이팅");
+
+            WriteLine();
+            WriteLine("소프트웨어");
+
+            Console.WriteLine(10 + 20 + 30 * 3);
+            */
+
+            /**
+             * 기본 자료형 : 정수
+            Console.WriteLine(50 + 273);
+            Console.WriteLine(10 / 2);
+            Console.WriteLine(10 % 3);
+            */
+
+            /**
+             * 연산자와 부호
+             
+            Console.WriteLine(4 % 3);
+            Console.WriteLine(-4 % 3);
+            */
+
+            /**
+             * 기본 자료형 : 실수
+             
+            Console.WriteLine(12.5 + 65.7);
+            Console.WriteLine(12.0 + 15.0);
+            Console.WriteLine(5.0 % 2.2); *실수에서는 나머지 연산 사용 안함
+            */
+
+            /**
+             * 기본 자료형 : 문자, 문자열
+            
+            Console.WriteLine('문');
+            Console.WriteLine("문자열");
+            
+             * 문자열 연결 연산자 (+)
+            
+            Console.WriteLine("가나다" + "라마" + "바사아");
+            Console.WriteLine(253 + "가나");
+            */
+
+            /**
+             * 
+             */
+        }
+
+    }
+}
