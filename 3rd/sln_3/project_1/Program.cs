8ede2b4f7a09830e4bb8c105fed8afadb81b2b24
MCGom <100755492+MCGom@users.noreply.github.com>
2023-03-21 12:41:20 +0900

    Add files via upload

---
+++ 3rd/sln_3/project_1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //복합대입 연산자
+            /*int output = 0;
+            output += 52;   //output = output + 52
+            output += 273;
+
+            Console.WriteLine(output);*/
+
+            //+(문자열 연결 연산자)
+            /*
+            Console.WriteLine(10 + 10); //20
+            Console.WriteLine(10 + "10");   //1010
+            Console.WriteLine("소스" + "제어"); //소스제어
+            Console.WriteLine(10 + 'a');    //107
+            */
+
+            //증감 연산자 -> 시험 나옴 ㅅㄱ
+            /*
+            int number1 = 10;
+            number1++;
+            Console.WriteLine(number1); //11
+            number1--;
+            Console.WriteLine(number1); //10
+
+            int number2 = 20;
+            Console.WriteLine(number2); //20
+            Console.WriteLine(number2++);   //20 -> 21
+            Console.WriteLine(number2--);   //21 -> 20
+            Console.WriteLine(number2); //20
+
+            int number3 = 50;
+            Console.WriteLine(number3); //50
+            number3++;
+            number3++;
+            Console.WriteLine(number3); //52
+            
+
+            int number4 = 65;
+            Console.WriteLine(number4++);   //65 -> 66
+            Console.WriteLine(--number4);   //65
+            Console.WriteLine(number4++);  //65
+            Console.WriteLine(++number4);   //67
+            */
+
+            //자료형
+            /*
+            int _int = 273;
+            long _long = 522733103265;
+            float _float = 52.273F;
+            double _double = 52.273;
+            char _char = '글';
+            string _string = "문자열";
+            
+
+            //GetType() 메서드를 활용해서 자료형 출력
+            
+            Console.WriteLine(_int.GetType());
+            Console.WriteLine(_long.GetType());
+            Console.WriteLine(_float.GetType());
+            Console.WriteLine(_double.GetType());
+            Console.WriteLine(_char.GetType());
+            Console.WriteLine(_string.GetType());
+            */
+
+            //직접적인 GetType() 메서드를 활용해서 자료형 출력
+            Console.WriteLine((52.273F).GetType());
+
+            //var 키워드
+            //지역 변수로 선언 : 메서드 내부에 선언되어 있는 변수
+            //메서드 외부에 var 키워드를 사용하면 -> 오류 발생
+            //변수 선언과 동시에 초기화 선언
+
+            var global = 52;
+            var number = 100;
+
+
+
+
+        }
+    }
+}
---
+++ 3rd/sln_3/project_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //문자열 서식 맞추기 -> 시험에 나옴 ㅅㄱ
+            //console.WriteLine(""); -> 기본적으로 문자열을 출력하는 메소드
+            //숫자를 출력할 때는 문자열로 변환하는 과정이 필요
+            //C#은 문자열 서식화에 사용할 수 있는 간편한 방법 두 가지를 제공
+            //① string 형식의 Format() 메서드
+            //② 문자열 보간(Interpolation)
+            /*
+            Console.WriteLine("제목 : " + "컴소공");
+
+            //① string 형식의 Format() 메서드
+            Console.WriteLine("제목 : {0}", "컴소공");
+
+
+            //② 문자열 보간(Interpolation)
+            Console.WriteLine($"제목 : {"컴소공"}");
+
+            Console.WriteLine("{0}, {1}, {2}", 123, "최강컴소과", 52.273F);  //string 형싱의 Format()
+            Console.WriteLine($"{123}, {"최강컴소과"}, {52.273F}");  //문자열 보간
+            */
+
+            //{0, 맞춤, 서식 문자열} -> string 형식의 Format()
+
+            Console.WriteLine("Total : {0, 7:D}", 255); //첨자 : 0, 맞춤 : 7, 서식 문자열 : D(십진수)
+            Console.WriteLine("Total : {0, -7:D}", 255);
+
+            Console.WriteLine("{0, -10:D5}", 123);
+            Console.WriteLine($"{123,-10:D5}");
+
+            int n = 123;
+            string s = "최강 컴소과";
+            Console.WriteLine("{0}, {1}", n, s);
+            Console.WriteLine($"{n}, {s}");
+
+            string fmt = "{0, -20}, {1, -15}, {2, 30}";
+
+            Console.WriteLine(fmt, "Yuhan", "Computer", "Programming");
+
+        }
+    }
+}
