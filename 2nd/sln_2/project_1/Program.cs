05c132f951c6d6a869f6277bba469ce6b1ce2b8c
MCGom <100755492+MCGom@users.noreply.github.com>
2023-03-21 10:04:58 +0900

    Add files via upload

---
+++ 2nd/sln_2/project_1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using static System.Console;
+
+namespace project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("헬로우 월드");
+            Console.WriteLine("C# 프로그래밍");
+            Console.Write("실습");
+            Console.Write("화이팅");
+            WriteLine("소프트웨어");
+            Console.WriteLine(10/2+30*3);
+
+
+            // 기본 자료형 : 정수
+            Console.WriteLine(50 + 273);
+            Console.WriteLine(10 / 2);
+            Console.WriteLine(10 % 3); 
+
+            // 연산자의 부호
+            Console.WriteLine(4 % 3);
+            Console.WriteLine(-4 % 3);
+
+            // 실수
+            Console.WriteLine(12.5 + 65.7);
+            Console.WriteLine(12.0 + 15.0);
+            Console.WriteLine(5.0 % 2.2); //실수는 나머지 연산자 사용안함
+
+            // 문자, 문자열
+            Console.WriteLine('문');
+            Console.WriteLine("문자열");
+
+
+            // 문자열 연결 연산자 (+)
+            Console.WriteLine("가나다" + "라마" + "바사아");
+            Console.WriteLine(253 + "가나"); 
+
+            // 문자 선택 괄호
+            Console.WriteLine("안녕하세요.");
+            Console.WriteLine("안녕하세요."[3]); //세
+            // Console.WriteLine("안녕하세요."[6]); //오류
+            Console.WriteLine("안녕하세요."[5]); 
+
+            // 불 : 참과 거짓을 표현할 때 사용(불과 비교 연산자)
+            Console.WriteLine(52 == 273);
+            Console.WriteLine(52 < 273); // < 작다
+            Console.WriteLine(52 > 273); // > 크다 */
+
+            // 논리 연산자(논리부정 !, 논리합 ||, 논리곱 &&)
+            Console.WriteLine(!true);
+            Console.WriteLine(DateTime.Now.Hour < 3 || 8 < DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Hour < 3 && 8 < DateTime.Now.Hour);
+
+            // 변수: 값을 저장할때 사용하는 식별자
+            // 정수형 자료형(int, long)
+
+
+
+        }
+
+    }
+}
