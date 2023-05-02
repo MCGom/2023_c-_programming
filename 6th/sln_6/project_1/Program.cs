036fea6989b13967316f6fd9469b52cbfdd62df6
MCGom <100755492+MCGom@users.noreply.github.com>
2023-05-02 12:42:37 +0900

    Add files via upload

---
+++ 6th/sln_6/project_1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //반복문과 배열
+            /*
+            Console.WriteLine("Output");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Output");
+            }
+
+            */
+
+            /*
+            //int score = 60; 변수
+            int[] score = new int[5];
+            score[0] = 80;
+            score[1] = 70;
+            score[2] = 90;
+            score[3] = 40;
+            score[4] = 20;
+
+            //int[] scores = new int[5] { 80, 70, 90, 40, 20 };
+            //int[] scores = new int[] { 80, 70, 90, 40, 20 };
+            //int[] scores = { 80, 70, 90, 40, 20 };
+            
+
+            int[] score = { 80, 90, 10, 70, 20 };
+
+            foreach (var value in score)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine();
+
+            int sum = 0 ;
+
+            foreach (var value in score)
+            {
+                sum += value;
+            }
+            Console.WriteLine(sum);
+
+            //int avg = sum / 5;
+            int avg = sum / score.Length;
+
+            Console.WriteLine(avg);
+*/
+            int[] score_1 = new int[5] { 90, 70, 10, 40, 50 };
+            //new == 메모리 할당 연산자
+
+            //원하는 크기의 배열 선언 방법
+
+            int[] int_array = new int[100];
+            Console.WriteLine(int_array[0]); //일반적인 숫자 자료형은 0으로 초기화
+            List<int> int_list = new List<int>();
+
+            char[] char_array = new char[100];
+            Console.WriteLine(char_array[0]);//문자형 자료형은 빈 문자로 초기화
+
+            //Console.WriteLine(score_1[5]);
+            //Console.WriteLine(score_1[-4]);
+            Console.WriteLine(score_1.Length);
+
+
+        }
+
+    }
+}
---
+++ 6th/sln_6/project_2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //무한 반복
+            /*
+            for (; ;)
+            {
+                Console.WriteLine("무한 루프");
+            }
+
+            */
+            /*
+            while(true)
+            {
+                Console.WriteLine("무한 루프");
+            }
+            
+
+            //for 반복문 덧셈
+            int output = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                output += i;
+            }
+            Console.WriteLine(output);
+            
+            //for 반복문 곱셈
+            int output = 1;
+            for (int i = 1; i <= 10; i++)
+            {
+                output *= i;
+            }
+            Console.WriteLine(output);
+            
+
+            //역 for 문
+            int[] int_array = { 1, 2, 3, 4, 5, 6, 7 };
+            for (int i = int_array.Length -1 ; i >= 0 ; i--)
+            {
+                Console.WriteLine(int_array[i]);
+            }
+
+            string[] array = { "사과", "배", "바나나", "수박","복숭아" };
+            foreach (var item in array)
+            {
+                Console.Write(item);
+            }
+*/
+            //while 반복문
+            int i = 0;
+            int[] array = {27, 31, 4, 88, 91, 56 };
+            while(i < array.Length)
+            {
+                Console.WriteLine((i + 1) + " 번째 출력 : " + array[i]);
+                i++;
+            }
+
+            Console.WriteLine();
+
+            for (int j = 0; j < array.Length; j++)
+            {
+                Console.WriteLine((j + 1) + " 번째 출력 : " + array[j]);
+            }
+
+        }
+    }
+}
