d6170d1c3c04f79c0d70ddf8c1b737e428b46ac9
MCGom <100755492+MCGom@users.noreply.github.com>
2023-06-19 18:57:10 +0900

    Add files via upload

---
+++ Delegate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegate
+{
+    class Program
+    {
+        class Product
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+        }
+
+        static int SortWithPrice(Product a, Product b)
+        {
+            return a.Price.CompareTo(b.Price);
+        }
+
+        static void Main(string[] args)
+        {
+            //List를 생성
+            List<Product> products = new List<Product>()
+            {
+                new Product() {Name = "감 자", Price = 5000},
+                new Product() {Name = "사 과", Price = 7000},
+                new Product() {Name = "고구마", Price = 4000},
+                new Product() {Name = "배 추", Price = 6000},
+                new Product() {Name = "상 추", Price = 3000}
+            };
+
+            //정렬
+            products.Sort(SortWithPrice);
+
+            foreach (var item in products)
+            {
+                Console.WriteLine(item.Name + " : " + item.Price);
+            }
+
+        }
+    }
+}
---
+++ UnnamedDelegate/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UnnamedDelegate
+{
+    class Program
+    {
+        class Product
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            //List를 생성
+            List<Product> products = new List<Product>()
+            {
+                new Product() {Name = "감 자", Price = 5000},
+                new Product() {Name = "사 과", Price = 7000},
+                new Product() {Name = "고구마", Price = 4000},
+                new Product() {Name = "배 추", Price = 6000},
+                new Product() {Name = "상 추", Price = 3000}
+            };
+
+            //무명 델리게이트로 정렬 방법 정의
+            /*products.Sort(
+                delegate (Product a, Product b)
+                {
+                    return a.Price.CompareTo(b.Price);
+                }
+                );*/
+
+            // Sort() 메서드의 매개 변수로 람다를 지정
+            products.Sort((a, b) => a.Price.CompareTo(b.Price));
+
+            foreach (var item in products)
+            {
+                Console.WriteLine(item.Name + " : " + item.Price);
+            }
+
+        }
+    }
+}
